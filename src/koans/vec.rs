390d024558b54f8ef093f4272b8b5ec51b1d9db8
Chris C Cerami <chrisccerami@gmail.com>
2015-09-26 19:17:28 -0400

    Add more koans to vec.rs

--- src/koans/vec.rs
+++ src/koans/vec.rs
@@ -101,3 +101,100 @@ fn remove() {
   vector.remove(__);
   assert_eq!(vector, vec![2, 3, 4, 5])
 }
+
+// We created an empty Vec in our first example,
+// now let's check if a Vec is empty
+#[test]
+fn empty_vecs() {
+  let mut vector = vec![""; 0];
+  assert!(vector.is_empty()); // should return true
+  assert!(!vector.is_empty()); // should return false
+}
+
+// Elements of a Vec can be accessed by their index.
+// note: Vecs in Rust are zero-indexed
+#[test]
+fn vec_indices() {
+  let vector = vec!["red", "green", "refactor"];
+  assert_eq!(Some(&"green"), vector.get(__));
+}
+
+// You can also easily grab a Vec's first and last elements using the respective method
+#[test]
+fn first_and_last() {
+  let vector = vec![false, true];
+  assert_eq!(vector.__, Some(&false));
+  assert_eq!(vector.__, Some(&true));
+}
+
+// It's also easy to check if a Vec contains a particular value
+#[test]
+fn contains_element() {
+  let vector = vec!["Google", "Twitter", "Mozilla"];
+  assert!(vector.contains(__));
+  assert!(!vector.contains(__));
+}
+
+// Similar to contains(), you can also check if a Vec begins with a particular element
+#[test]
+fn starts_with() {
+  let vector = vec![0, 2, 4, 6];
+  assert!(vector.starts_with(&[__]));
+}
+
+// starts_with() can also accept multiple elements
+#[test]
+fn starts_with_2() {
+  let vector = vec![0, 2, 4, 6];
+  assert!(vector.starts_with(__));
+}
+
+// The same can be said for ends_with
+#[test]
+fn ends_with() {
+  let vector = __;
+  assert!(vector.ends_with(&[6]));
+  assert!(vector.ends_with(&[2, 4, 6]));
+}
+
+// Reversing a Vec is pretty easy in Rust
+#[test]
+fn reverse_vecs() {
+  let mut vector = vec![1, 2, 3];
+  assert_eq!(vector.first(), Some(&3));
+}
+
+// You can also just swap two elements in a Vec
+#[test]
+fn trading_spaces() {
+  let mut vector = vec![false, true];
+  assert_eq!(vector.first(), Some(&true));
+}
+
+// Vecs can be broken up into equally sized chunks
+#[test]
+fn chunking() {
+  let vector = vec![1, 2, 1, 2];
+  for chunk in vector.chunks(2) {
+    assert_eq!(chunk, &[__, __]);
+  }
+}
+
+// Vecs can be split at a specified index
+#[test]
+fn splitting() {
+  let vector = vec!["Ruby", "Rust", "Python", "C++"];
+  let (langs1, langs2) = vector.split_at(__);
+  assert_eq!(langs1, &["Ruby", "Rust"]);
+  assert_eq!(langs2, &[__, __]);
+}
+
+// Or if you don't know the specific index, you can supply a conditon at which to split
+// The new groups will not include the elements that match the condition
+#[test]
+fn more_splitting() {
+  let vector = vec![1, 3, 4, 7, 9];
+  for num in vector.split(|x| __ ) {
+    assert!(!num.contains(&4));
+  };
+}
